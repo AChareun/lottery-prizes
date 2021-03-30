0f3edb449b71cd8437f136bde7a7b1c9b92b173a
AChareun <achareun22@gmail.com>
2021-03-30 17:01:07 -0300

    implement apiResponse module and error dictionary

---
+++ server/src/lib/apiResponse.ts
@@ -0,0 +1,36 @@
+import { ApiErrors } from '../core/apiError';
+
+export interface IApiResponse {
+    status: string;
+    code: number;
+    message: string;
+    data: any | any[] | null;
+}
+
+export class ApiResponseHelper {
+    apiErrors: Error[];
+
+    constructor(apiErrors: Error[]) {
+        this.apiErrors = apiErrors;
+    }
+
+    buildOkResponse(data?: any | any[]): IApiResponse {
+        return {
+            status: 'OK',
+            code: 200,
+            message: 'Success on request',
+            data: data || null,
+        };
+    }
+
+    buildErrorResponse(errorName: string): IApiResponse {
+        const apiError = ApiErrors[errorName];
+
+        return {
+            status: 'ERROR',
+            code: apiError.code,
+            message: apiError.message,
+            data: null,
+        };
+    }
+}
