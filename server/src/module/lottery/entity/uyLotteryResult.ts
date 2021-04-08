ce25c571235f67b8cc031cd47fd5a60417f189b8
AChareun <achareun22@gmail.com>
2021-04-08 09:05:30 -0300

    implement enums for lottery names and types and a lottery interface with classes that implements it

---
+++ server/src/module/lottery/entity/uyLotteryResult.ts
@@ -0,0 +1,37 @@
+import { ELotteryNames, ELotteryTypes, ILotteryResult, TResult } from './iLotteryResult';
+
+export class UyLotteryResult implements ILotteryResult {
+    id: number | null;
+    name: keyof typeof ELotteryNames;
+    results: TResult[];
+    type: keyof typeof ELotteryTypes;
+    date: Date;
+
+    constructor(
+        name: keyof typeof ELotteryNames,
+        type: keyof typeof ELotteryTypes,
+        results: any,
+        date: Date,
+        id: number | null = null
+    ) {
+        this.id = id;
+        this.date = date;
+        this.name = name;
+        this.type = type;
+        this.results = this.formatResults(results);
+    }
+
+    formatResults(results: { order: number; result: number }[]): TResult[] {
+        return results.map(({ order, result }) => {
+            const strResult =
+                result === null || typeof result === 'undefined'
+                    ? null
+                    : String(result).padStart(3, '0');
+
+            return {
+                position: order,
+                result: strResult,
+            };
+        });
+    }
+}
