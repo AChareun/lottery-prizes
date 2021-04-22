bdbd65f62f637c5309abbec1efd1b883889e7151
AChareun <achareun22@gmail.com>
2021-04-22 10:14:41 -0300

    refactor app to be a runnable script that can receive args to determine which data to scrap

--- scraper/src/app.ts
+++ scraper/src/app.ts
@@ -1,32 +1,22 @@
 require('dotenv').config();
-require('debug')('lottery-scraper');
 
-import { SanDiegoSource } from './sources/sandiego';
+import { getConfigureDIC } from './config/dic';
+import { ScrapingController } from './module/scraping/controller/scrapingController';
 
-function formatData(data: any[]) {
-    let log = '';
-    data.forEach((e) => {
-        let eLog = '';
-        eLog += e.title + '\n';
-        const { numbers } = e;
-        for (let i = 0; i < numbers.length / 2; i++) {
-            const [numberA, numberB] = [numbers[i], numbers[i + 10]];
-            eLog += `|${numberA.order}| ${numberA.number} || ${numberB.order}| ${numberB.number}|\n`;
+const main = () =>
+    new Promise<string>(async (res, rej) => {
+        const [requiredDate, requiredType, requiredTurn] = [process.argv[2], process.argv[3], process.argv[4]];
+
+        const scrapingController = getConfigureDIC().get<ScrapingController>('ScrapingController');
+
+        try {
+            const requiredData = await scrapingController.requestData(requiredDate, requiredType, requiredTurn);
+            res(JSON.stringify(requiredData));
+        } catch (error) {
+            rej(error);
         }
-
-        log += eLog + '\n';
     });
 
-    return log;
-}
-
-async function main() {
-    const source = new SanDiegoSource(null);
-
-    const [nodePath, scriptPath, date, type, turn] = process.argv;
-    const results = await source.getData(date, type, turn);
-
-    return formatData(results);
-}
-
-main().then((r) => console.log(r));
+main()
+    .then((r: string) => console.log(r))
+    .catch((err: Error) => console.error(err));
